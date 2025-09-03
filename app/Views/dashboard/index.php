d812225642abd05f0ea06c292ab7df521bf2870b
fredericopaulista <fredericopaulista@gmail.com>
2025-09-02 22:35:45 -0300

    inndex dashboard sem empresa

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -10,7 +10,7 @@
 <?php echo $this->section('content'); ?>
 
 <div class="row">
-    <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+    <div class="col-xl-6 col-sm-6 mb-xl-0 mb-4">
         <div class="card">
             <div class="card-body p-3">
                 <div class="row">
@@ -31,7 +31,7 @@
             </div>
         </div>
     </div>
-    <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+    <div class="col-xl-6 col-sm-6 mb-xl-0 mb-4">
         <div class="card">
             <div class="card-body p-3">
                 <div class="row">
@@ -53,7 +53,7 @@
             </div>
         </div>
     </div>
-    <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
+    <!-- <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
         <div class="card">
             <div class="card-body p-3">
                 <div class="row">
@@ -74,7 +74,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
     <!-- <div class="col-xl-3 col-sm-6">
         <div class="card">
             <div class="card-body p-3">
