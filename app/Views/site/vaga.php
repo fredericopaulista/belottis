1d317933d12b04c221f2152d61af3e2ffc3bef93
fredericopaulista <fredericopaulista@gmail.com>
2025-09-02 22:48:07 -0300

    seo title vaga

--- app/Views/site/vaga.php
+++ app/Views/site/vaga.php
@@ -4,11 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php echo $this->section('title'); ?>
-    <?php echo $$data->title ?? 'Vaga de Estágio'; ?>
-    <?php echo $this->endSection(); ?>
+    <title><?php echo $title ?? 'Vaga | Belottis' ; ?></title>
+
     <meta name="description"
-        content="Detalhes completos da vaga de emprego. Confira os requisitos, benefícios e como se candidatar.">
+        content="<?php echo $data->meta_description ?? 'Detalhes completos da vaga de emprego. Confira os requisitos, benefícios e como se candidatar.'; ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
     * {
