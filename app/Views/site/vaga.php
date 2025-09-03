10c24e7083bf66172cf2f88998eeaa893e9fa82d
fredericopaulista <fredericopaulista@gmail.com>
2025-09-02 22:43:38 -0300

    title vaga

--- app/Views/site/vaga.php
+++ app/Views/site/vaga.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Belottis - Detalhes da Vaga</title>
+    <?php echo $this->section('title'); ?>
+    <?php echo $page_title ?? 'Vaga de Eestágio'; ?>
+    <?php echo $this->endSection(); ?>
     <meta name="description"
         content="Detalhes completos da vaga de emprego. Confira os requisitos, benefícios e como se candidatar.">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
@@ -879,7 +881,7 @@
 
                                     <div class="job-location"><?= $vaga->cidade ?>, <?= $vaga->estado ?></div>
                                     <div class="job-tags">
-                                        <!-- <span class="job-tag"><?= $vaga->tipo_contratacao ?></span> -->
+
                                         <span class="job-tag"><?= $vaga->modalidade ?></span>
                                         <span class="job-tag"><?= $vaga->nivel_experiencia ?></span>
                                     </div>
